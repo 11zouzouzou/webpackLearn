8478913348a0e8ba6e7581ad8d27a96dfaf31166
zouzouzou11 <1096174991@qq.com>
2020-09-06 16:44:18 +0800

    feat:管理资源

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 #learn webpackv4.x
+
 #init
-#npm init -y ; yarn add webpack webpack --save-dev
+
+npm init -y ; 
+yarn add webpack webpack --save-dev ; 
+新建webpack.config.js;
+
+#管理资源
+
+yarn add css-loader style-loader file-loader csv-loader xml-loader;(CSS ,图片文件加载，csv\XML加载)；
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from "lodash";
+import Icon from "./test.png";
+import "./style.css";
+import Data from "./data.xml";
 function component() {
   const element = document.createElement("div");
 
   // lodash ES6
   console.log("1");
   element.innerHTML = _.join(["Hello1", "webpack"], " ");
+  element.classList.add("hello"); //add css
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data); //打印info
 
   return element;
 }
