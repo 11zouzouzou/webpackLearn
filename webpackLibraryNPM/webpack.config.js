37b43853a41a9032908cc6c6ffe7d255be6dc3fc
zouzouzou11 <1096174991@qq.com>
2020-09-19 17:30:04 +0800

    feat:环境变量

--- webpackLibraryNPM/webpack.config.js
+++ webpackLibraryNPM/webpack.config.js
@@ -1,22 +1,27 @@
 const path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "webpack-numbers.js",
-    libraryTarget: "umd",
-    // globalObject: "this",//暴露 library
-    // libraryExport: 'default',
-    library: "webpackNumbers",
-  },
-  externals: {
-    lodash: {
-      commonjs: "lodash", //忽略包
-      commonjs2: "lodash",
-      amd: "lodash",
-      root: "_",
+module.exports = (env) => {
+  console.log("node_env", env.NODE_ENV);
+  console.log("product", env.production);
+
+  return {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "webpack-numbers.js",
+      libraryTarget: "umd",
+      // globalObject: "this",//暴露 library
+      // libraryExport: 'default',
+      library: "webpackNumbers",
     },
-  },
+    externals: {
+      lodash: {
+        commonjs: "lodash", //忽略包
+        commonjs2: "lodash",
+        amd: "lodash",
+        root: "_",
+      },
+    },
+  };
 };
